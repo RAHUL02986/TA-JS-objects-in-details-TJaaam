94e09b1b722502373115904fcebf51daa52f6d44
Rahul kapoor <kapoorrahul304@gmail.com>
2023-03-21 12:59:47 +0530

    some changes in this assign

--- block-BJaail/index.js
+++ block-BJaail/index.js
@@ -5,7 +5,7 @@ const nameElm = form.elements.title;
 const authorElm = form.elements.author;
 const imgElm = form.elements.img;
 
-
+ 
 
 class Book {
     constructor (name, author, img) {
@@ -13,6 +13,8 @@ class Book {
         this.author = author;
         this.img = img;
         this.isRead = false;
+        this.books.push(book);
+        this.createUI();
     }
     toggleIsRead() {
         this.isRead = !this.isRead;
@@ -22,13 +24,15 @@ class Book {
 class BookList {
     constructor (books = []) {
         this.books = books;
+        this.createUI();
     }
-    addBook(name, author, img) {
-        let book = new Book(name, author, img);
+    addBook(name,author,img){
+        let book = new book(name,author,img);
         this.books.push(book);
         this.createUI();
-    } 
-    createUI() {
+    }
+
+        createUI(){
         rootBook.innerHTML = '';
         this.books.forEach((book) => {
             let li = document.createElement("li");
@@ -37,19 +41,26 @@ class BookList {
             let h1 = document.createElement("h1");
             h1.innerText = book.name;
             let p = document.createElement("p");
-            p.innerText = book.author
+            p.innerText = book.author;
             let button = document.createElement("button");
-            button.addEventListener('click', () => {
+            button.innerText = "Mark is Read!";
+            button.classList.add(".read_btn");
+            button.innerText = book.isRead ? 'Completed!' : 'Mark as read!';
+
+            button.addEventListener('click', ()=>{
                 book.toggleIsRead();
                 this.createUI();
             });
-            button.BookList.add("read_btn");
-            button.innerText = book.isRead ? 'Completed!' : 'Mark as read!';
             li.append(img, h1, p, img, button);
             rootBook.append(li);
-        });        
+
+            });
+        
     }
+
 }
+
+
 
 let library = new BookList();
 
@@ -64,3 +75,5 @@ function handleSubmit(event) {
     authorElm.value = "";
     imgElm.value = "";
 }
+
+form.addEventListener('AddBook',handleSubmit);
